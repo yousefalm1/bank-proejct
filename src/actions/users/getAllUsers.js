1bd3a31dec8f7afecc3c48bf0c7767ac1d8c874a
f h <falhamad2002@gmail.com>
2024-10-28 11:34:57 +0300

    getAllUsers is done

--- src/actions/users/getAllUsers.js
+++ src/actions/users/getAllUsers.js
@@ -1,1 +1,13 @@
 // Retrieves a list of all users and their account information using a GET request.
+"use server";
+import { baseUrl, getHeaders } from "@/actions/config";
+
+export async function GetAllUsers() {
+  const response = await fetch(`${baseUrl}/mini-project/api/auth/users`, {
+    method: "GET",
+    headers: await getHeaders(),
+  });
+  console.log(response);
+  const users = await response.json();
+  return users;
+}
